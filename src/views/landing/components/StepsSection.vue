d6f272d5db9a9ca85175deba39d7c72ccf07fd5f
Jetross <jetrossneri07@gmail.com>
2026-03-26 13:08:32 +0800

    feat: add card component

---
+++ src/views/landing/components/StepsSection.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineOptions({
+  name: 'StepsSection',
+})
+</script>
+
+<template>
+  <section class="w-full bg-far py-20 md:py-28">
+    <div class="mx-auto w-full max-w-screen-xl px-6 md:px-10">
+      <!-- Heading -->
+      <div class="mx-auto mb-14 max-w-4xl text-center md:mb-16"></div>
+    </div>
+  </section>
+</template>
