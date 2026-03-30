6353dbd81490167edf5d2523bbdb33001309b7a5
Jetross <jetrossneri07@gmail.com>
2026-03-30 09:39:38 +0800

    feat: add UsersView

---
+++ src/views/(admin)/users/UsersView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-full">
+    <h1 class="text-2xl font-bold mb-4">Users Management</h1>
+    <!-- Add your user management components here -->
+  </div>
+</template>
+
+<style scoped></style>
