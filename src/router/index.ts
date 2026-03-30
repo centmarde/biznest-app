e1d57238c071b2bf70e38073e129d90caab65795
Jetross <jetrossneri07@gmail.com>
2026-03-30 09:43:22 +0800

    feat: add UsersView on router

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,14 @@ import OuterLayout from '@/layouts/OuterLayout.vue'
 import AuthLayout from '@/layouts/AuthLayout.vue'
 import InnerLayout from '@/layouts/InnerLayout.vue'
 import LandingView from '@/views/landing/LandingView.vue'
+
+//Auth Routes
 import LoginView from '@/views/auth/login/LoginView.vue'
 import RegisterView from '@/views/auth/register/RegisterView.vue'
+
+//Admin Routes
 import TestView from '@/views/test/TestView.vue'
+import UsersView from '@/views/(admin)/users/UsersView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -49,6 +54,12 @@ const router = createRouter({
           path: '',
           name: 'test',
           component: TestView,
+          meta: { requiresAuth: true },
+        },
+        {
+          path: 'users',
+          name: 'users',
+          component: UsersView,
           meta: { requiresAuth: true },
         },
       ],
